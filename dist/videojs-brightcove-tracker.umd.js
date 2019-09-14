6bd58d1eb3b01a7af271e3b8cd212bb2eceded5a
Indra Santosa <indrasantosa90@gmail.com>
2019-09-15 03:43:09 +0800

    inject data to brightcove object

--- dist/videojs-brightcove-tracker.umd.js
+++ dist/videojs-brightcove-tracker.umd.js
@@ -5,55 +5,87 @@ var howLongUntilLunch = (function (videojs) {
 
   var version = "0.0.1";
 
+  const trackingUrlSample =
+    'http://metrics.brightcove.com/tracker?event=video_view&session=581136_2018-07-03T18:34:46.214Z&destination=http%3A%2F%2Fwww.current-times.com%2F&video=2621468623001&video_name=Debate-2&video_duration=189&time=1377191666432&source=http%3A%2F%2Fwww.google.com%2Furl%252F%26ei%3DoEYWUtCgEIXq9ATznoCgCQ%26us-g%3DAFQjCNEtv.51156542%2Cd.dmg&domain=videocloud&account=1749339200';
+
   // Cross-compatibility for Video.js 5 and 6.
   const registerPlugin = videojs.registerPlugin || videojs.plugin;
   const dom = videojs.dom || videojs;
 
-  const EVENTS = [
-    'loadstart',
-    'progress',
-    'suspend',
-    'abort',
-    'error',
-    'emptied',
-    'stalled',
-    'loadedmetadata',
-    'loadeddata',
-    'canplay',
-    'canplaythrough',
-    'playing',
-    'waiting',
-    'seeking',
-    'seeked',
-    'ended',
-    'durationchange',
-    'timeupdate',
-    'play',
-    'pause',
-    'ratechange',
-    'resize',
-    'volumechange'
-  ];
+  class BrightcoveTracker {
+    constructor({
+      player,
+      session,
+      destination,
+      video,
+      videoName,
+      videoDuration,
+      time,
+      source,
+      domain,
+      account
+    }) {
+      const self = this;
+      this.player = player;
 
-  const trackingUrlSample =
-    'http://metrics.brightcove.com/tracker?event=video_view&session=581136_2018-07-03T18:34:46.214Z&destination=http%3A%2F%2Fwww.current-times.com%2F&video=2621468623001&video_name=Debate-2&video_duration=189&time=1377191666432&source=http%3A%2F%2Fwww.google.com%2Furl%252F%26ei%3DoEYWUtCgEIXq9ATznoCgCQ%26us-g%3DAFQjCNEtv.51156542%2Cd.dmg&domain=videocloud&account=1749339200';
+      this.session = session;
+      this.destination = destination;
+      this.video = video;
+      this.videoName = videoName;
+      this.videoDuration = videoDuration;
+      this.time = time;
+      this.source = source;
+      this.domain = domain;
+      this.account = account;
 
-  const onPlayerLoadedMetaData = e => player => {
-    const el = dom.createEl('img', {
-      className: 'vcs-bctr'
-    });
-    el.src = trackingUrlSample;
-    player.el().appendChild(el);
-  };
+      this.sendTracking = this.sendTracking.bind(this);
+
+      this.player.on('loadedmetadata', function(e) {
+        self.sendTracking();
+      });
+    }
+
+    setVideoTracker({
+      session = this.session,
+      destination = this.destination,
+      video = this.video,
+      videoName = this.videoName,
+      videoDuration = this.videoDuration,
+      time = this.time,
+      source = this.source,
+      domain = this.domain,
+      account = this.account
+    }) {
+      this.session = session;
+      this.destination = destination;
+      this.video = video;
+      this.videoName = videoName;
+      this.videoDuration = videoDuration;
+      this.time = time;
+      this.source = source;
+      this.domain = domain;
+      this.account = account;
+    }
+
+    sendTracking() {
+      /**
+       * Based on best practice, we need to send analytics by creating an
+       * image with source the tracking url. Pretty much
+       * ref: https://support.brightcove.com/overview-data-collection-api-v2
+       */
+      const el = dom.createEl('img', {
+        className: 'vcs-bctr'
+      });
+      el.src = trackingUrlSample;
+      this.player.el().appendChild(el);
+    }
+  }
 
   function brightcoveTracker(options) {
-    EVENTS.map(event => {
-      this.on(event, function(e) {
-        console.log(`Event ${event} is triggered`);
-      });
-
-      this.on('loadedmetadata', function(e) {
-        onPlayerLoadedMetaData()(this);
+    this.ready(() => {
+      this.bcTracker = new BrightcoveTracker({
+        ...options,
+        player: this
       });
     });
   }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import videojs from 'video.js';
 import { version as VERSION } from '../package.json';
 
+const trackingUrlSample =
+  'http://metrics.brightcove.com/tracker?event=video_view&session=581136_2018-07-03T18:34:46.214Z&destination=http%3A%2F%2Fwww.current-times.com%2F&video=2621468623001&video_name=Debate-2&video_duration=189&time=1377191666432&source=http%3A%2F%2Fwww.google.com%2Furl%252F%26ei%3DoEYWUtCgEIXq9ATznoCgCQ%26us-g%3DAFQjCNEtv.51156542%2Cd.dmg&domain=videocloud&account=1749339200';
+
 // Cross-compatibility for Video.js 5 and 6.
 const registerPlugin = videojs.registerPlugin || videojs.plugin;
 const dom = videojs.dom || videojs;
@@ -31,25 +34,80 @@ const EVENTS = [
   'volumechange'
 ];
 
-const trackingUrlSample =
-  'http://metrics.brightcove.com/tracker?event=video_view&session=581136_2018-07-03T18:34:46.214Z&destination=http%3A%2F%2Fwww.current-times.com%2F&video=2621468623001&video_name=Debate-2&video_duration=189&time=1377191666432&source=http%3A%2F%2Fwww.google.com%2Furl%252F%26ei%3DoEYWUtCgEIXq9ATznoCgCQ%26us-g%3DAFQjCNEtv.51156542%2Cd.dmg&domain=videocloud&account=1749339200';
+class BrightcoveTracker {
+  constructor({
+    player,
+    session,
+    destination,
+    video,
+    videoName,
+    videoDuration,
+    time,
+    source,
+    domain,
+    account
+  }) {
+    const self = this;
+    this.player = player;
 
-const onPlayerLoadedMetaData = e => player => {
-  const el = dom.createEl('img', {
-    className: 'vcs-bctr'
-  });
-  el.src = trackingUrlSample;
-  player.el().appendChild(el);
-};
+    this.session = session;
+    this.destination = destination;
+    this.video = video;
+    this.videoName = videoName;
+    this.videoDuration = videoDuration;
+    this.time = time;
+    this.source = source;
+    this.domain = domain;
+    this.account = account;
+
+    this.sendTracking = this.sendTracking.bind(this);
+
+    this.player.on('loadedmetadata', function(e) {
+      self.sendTracking();
+    });
+  }
+
+  setVideoTracker({
+    session = this.session,
+    destination = this.destination,
+    video = this.video,
+    videoName = this.videoName,
+    videoDuration = this.videoDuration,
+    time = this.time,
+    source = this.source,
+    domain = this.domain,
+    account = this.account
+  }) {
+    this.session = session;
+    this.destination = destination;
+    this.video = video;
+    this.videoName = videoName;
+    this.videoDuration = videoDuration;
+    this.time = time;
+    this.source = source;
+    this.domain = domain;
+    this.account = account;
+  }
+
+  sendTracking() {
+    /**
+     * Based on best practice, we need to send analytics by creating an
+     * image with source the tracking url. Pretty much
+     * ref: https://support.brightcove.com/overview-data-collection-api-v2
+     */
+    const el = dom.createEl('img', {
+      className: 'vcs-bctr'
+    });
+    el.src = trackingUrlSample;
+    this.player.el().appendChild(el);
+  }
+}
 
 function brightcoveTracker(options) {
-  EVENTS.map(event => {
-    this.on(event, function(e) {
-      console.log(`Event ${event} is triggered`);
-    });
-
-    this.on('loadedmetadata', function(e) {
-      onPlayerLoadedMetaData(e)(this);
+  this.ready(() => {
+    this.bcTracker = new BrightcoveTracker({
+      ...options,
+      player: this
     });
   });
 }
